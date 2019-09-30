e97409338297deff71c7e0e307d12801ffd98f78
swallace <sean.d.wallace@wsu.edu>
2019-09-29 19:59:25 -0700

    Added CommentCountTest and added Shotgun Surgery to the CommentCheck

---
+++ PluginProject/src/StructuralMetrics/MaxNumLoopsTest.java
@@ -0,0 +1,16 @@
+package StructuralMetrics;
+
+import org.junit.Assert;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+
+import static org.mockito.Mockito.*;
+
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+public class MaxNumLoopsTest {
+
+}
