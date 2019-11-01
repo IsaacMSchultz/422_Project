a2d8d5874be83faa952cf6fec0b65991507dc105
Isaac <isaac.schultz@wsu.edu>
2019-10-31 20:50:05 -0700

    Added fault test files for #19 LocalMethodCheck, NumberOfCasts

---
+++ PluginProject/BlackBoxTestCases/NumberOfCastsCheck/NumberOfCastsCheck2.java
@@ -0,0 +1,12 @@
+// This test case covers when there are external method calls and internal method calls.
+// Total of: 3
+
+class NumberOfCastsCheck2 { // total of: 3
+    NumberOfCastsCheck2() {
+        int a = 0, b = 1, c = 2;
+        double d = 1.2, e = 3.5, f = 34.1;
+        // a = (int) d;
+        // e = (double) b;
+        // c = a;
+    }
+}
---
+++ PluginProject/BlackBoxTestCases/NumberOfCastsCheck/NumberOfCastsCheck3.java
@@ -0,0 +1,15 @@
+// This test case covers when there are external method calls and internal method calls.
+// Total of: 3
+
+class NumberOfCastsCheck3 { // total of: 3
+    NumberOfCastsCheck3() {
+        int a = 0, b = 1, c = 2;
+        double d = 1.2, e = 3.5, f = 34.1;
+        a = (int) d;
+        e = (double) b;
+        c = a;
+        // a = (int) d;
+        // e = (double) b;
+        // c = a;
+    }
+}
